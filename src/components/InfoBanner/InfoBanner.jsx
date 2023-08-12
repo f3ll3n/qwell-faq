5d1b3700045636535103b882744c80a5386c8eff
Данила Корж <danila.schults@yandex.ru>
2023-08-12 21:35:54 +0300

    pre-deploy changes

--- src/components/InfoBanner/InfoBanner.jsx
+++ src/components/InfoBanner/InfoBanner.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import style from './InfoBanner.module.scss';
+import React from "react";
+import style from "./InfoBanner.module.scss";
 
 export const InfoBanner = () => {
   return (
     <div className={style.InfoBanner}>
-        <div>
-              Этот <strong>FAQ</strong> содержит ответы на основные вопросы о нашей платформе.
-        </div>
-        <br/>
-        <div>
-              Мы постоянно развиваемся и добавляет новые функции, поэтому этот документ <span className={style.red}>может</span> содержать устаревшую информацию.
-        </div>
+      <div>
+        Этот <strong>FAQ</strong> содержит ответы на основные вопросы о нашей
+        платформе.
+      </div>
+      <br />
+      <div>
+        Мы постоянно развиваемся и добавляет новые функции, поэтому этот
+        документ <span className={style.red}>может</span> содержать устаревшую
+        информацию.
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,57 +1,56 @@
-import React, { useState } from 'react'
-import classNames from 'classnames';
-import style from './ProgressBar.module.scss';
-import svg1 from './svg/svg1.svg';
-import svg2 from './svg/svg2.svg';
-import svg3 from './svg/svg3.svg';
+import React, { useState } from "react";
+import classNames from "classnames";
+import style from "./ProgressBar.module.scss";
+import svg1 from "./svg/svg1.svg";
+import svg2 from "./svg/svg2.svg";
+import svg3 from "./svg/svg3.svg";
 
 export const ProgressBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOnToggleMenu = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   return (
     <div className={style.ProgressBar}>
-        <div className={style.inner_progressbar}>
-            <div className={style.progressbar_text}>
-                Работа - это просто FAQ
-            </div>
-            <div onClick={() => handleOnToggleMenu()} className={style.progressbar_toggle_button}>
-                <div className={style.circle}></div>
-                <div className={style.circle}></div>
-                <div className={style.circle}></div>
-            </div>
+      <div className={style.inner_progressbar}>
+        <div className={style.progressbar_text}>Работа - это просто FAQ</div>
+        <div
+          onClick={() => handleOnToggleMenu()}
+          className={style.progressbar_toggle_button}
+        >
+          <div className={style.circle}></div>
+          <div className={style.circle}></div>
+          <div className={style.circle}></div>
         </div>
-      <div className={classNames(style.toggle_menu, isOpen ? null : style.hidden)}>
-          <div className={style.toggle_menu_transition}>
-            <div className={style.rounded_figure}>
-              <div className={style.circle_layer}></div>
-            </div>
-            <div className={style.figure}></div>
-        </div>
-          <div className={style.toggle_menu_nav}>
-            <nav className={style.navbar}>
-              <li className={style.navbar_item}>
-                <img src={svg3} alt='svg' />
-                <span>Поиск</span>
-                </li>
-              <li className={style.navbar_item}>
-                <img src={svg2} alt='svg' />
-                <span>
-                  Поделиться
-                </span>
-                </li>
-              <li className={style.navbar_item}>
-                <img src={svg1} alt='svg' />
-                <span>
-                  Открыть в ...
-                </span>
-               </li>
-            </nav>
+      </div>
+      <div
+        className={classNames(style.toggle_menu, isOpen ? null : style.hidden)}
+      >
+        <div className={style.toggle_menu_transition}>
+          <div className={style.rounded_figure}>
+            <div className={style.circle_layer}></div>
           </div>
+          <div className={style.figure}></div>
         </div>
+        <div className={style.toggle_menu_nav}>
+          <nav className={style.navbar}>
+            <li className={style.navbar_item}>
+              <img src={svg3} alt="svg" />
+              <span>Поиск</span>
+            </li>
+            <li className={style.navbar_item}>
+              <img src={svg2} alt="svg" />
+              <span>Поделиться</span>
+            </li>
+            <li className={style.navbar_item}>
+              <img src={svg1} alt="svg" />
+              <span>Открыть в ...</span>
+            </li>
+          </nav>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
